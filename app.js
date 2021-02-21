f736cee4fba9bec8fa2eeb20fe6408451b1706a7
Jonas Albaira <jonas.albaira@uoit.net>
2021-02-20 21:18:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
     res.end();
-}).listen(80);
+}).listen(3000);
 
 
 
